cb53c39f25479267cd22ef6af7d0d74ef8951bba
朱正阳 <770665184@qq.com>
2023-02-16 11:45:35 +0800

    1

--- config.js
+++ config.js
@@ -3,8 +3,8 @@ module.exports = {
 	name: "融汇商城",
 	// 必填: 后端api地址, 斜杠/结尾, 参照下面格式
 	// 例如: https://www.你的域名.com/index.php?s=/api/
-	// apiUrl: "https://dev.gzrhhj.com/index.php?s=/api/"
+	apiUrl: "https://dev.gzrhhj.com/index.php?s=/api/"
 	// apiUrl: "https://stdev.gzrhhj.com/index.php?s=/api/"
 	// apiUrl: "https://advert.gzrhhj.com/index.php?s=/api/"
-	apiUrl: "https://shop.gzrhhj.com/index.php?s=/api/"
+	// apiUrl: "https://shop.gzrhhj.com/index.php?s=/api/"
 }
